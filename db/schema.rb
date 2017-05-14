8be6356370dfe1d4fc04059d372dc921d8629dac
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-05-14 00:35:25 -0700

    plpgsql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170514014500) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "entries", force: :cascade do |t|
     t.string "phrase"
     t.integer "points"
